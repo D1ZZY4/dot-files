025913381f5420bf57f12c0c48c62f9fa5d5285d
D1ZZY4 <176969112+D1ZZY4@users.noreply.github.com>
2026-08-17 23:38:18 +0700

    fix(fish): revert broken prompt preview cache (Fish assoc array key limits)

--- config/fish/functions/dot-files/__dotfiles_print_prompt_preview.fish
+++ config/fish/functions/dot-files/__dotfiles_print_prompt_preview.fish
@@ -1,15 +1,6 @@
-# Cache rendered prompt per style+color to avoid 9 starship spawns per catalog.
-if not set -q __dotfiles_prompt_cache
-    set -g __dotfiles_prompt_cache
-end
-
 function __dotfiles_print_prompt_preview --argument-names label
     echo "  $label"
-    set -l cache_key "$STARSHIP_STYLE_PREVIEW:$STARSHIP_COLOR_PREVIEW"
-    if not set -q __dotfiles_prompt_cache[$cache_key]
-        set -g __dotfiles_prompt_cache[$cache_key] (__dotfiles_starship_prompt_block)
-    end
-    for line in (string split \n "$__dotfiles_prompt_cache[$cache_key]")
+    for line in (string split \n (__dotfiles_starship_prompt_block))
         echo "    $line"
     end
 end
